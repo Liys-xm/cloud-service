0af103b8e4bf30dd85f6c806b583356febd25448
AmyLi <630603232@qq.com>
2018-12-24 15:07:46 +0800

    网关系统

--- gateway-zuul/src/main/java/com/cloud/gateway/GatewayApplication.java
+++ gateway-zuul/src/main/java/com/cloud/gateway/GatewayApplication.java
@@ -7,20 +7,29 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 /**
  * 网关系统
+ * <p>
+ * Zuul 的过滤器 ZuulFilter 的使用。
+ * 注意 EnableZuulProxy 注解能注册到 eureka 服务上，是因为该注解包含了 eureka 客户端的注解，该
+ * EnableZuulProxy 是一个复合注解。
  *
  * @author liyaoshi
+ * @EnableZuulProxy --> { @EnableCircuitBreaker、@EnableDiscoveryClient } 包含了 eureka 客
+ * 户端注解，同时也包含了 Hystrix 断路器模块注解。
+ * http://localhost:8150/routes 地址可以查看该zuul微服务网关代理了多少微服务的serviceId。
  */
 @EnableFeignClients
 @EnableZuulProxy
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableScheduling
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+        System.out.println("【【【【【【 GatewayZuulFilter微服务 】】】】】】已启动.");
     }
 
 }
--- gateway-zuul/src/main/java/com/cloud/gateway/filter/BlackIPAccessFilter.java
+++ gateway-zuul/src/main/java/com/cloud/gateway/filter/BlackIPAccessFilter.java
@@ -17,6 +17,8 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
 /**
+ * zuul 的过滤器
+ * <p>
  * 黑名单IP拦截<br>
  * 黑名单ip变化不会太频繁，<br>
  * 考虑到性能，我们不实时掉接口从别的服务获取了，<br>
@@ -32,6 +34,11 @@ public class BlackIPAccessFilter extends ZuulFilter {
      */
     private Set<String> blackIPs = new HashSet<>();
 
+    /**
+     * 是否执行该过滤器。
+     * true：说明需要过滤；
+     * false：说明不要过滤；
+     */
     @Override
     public boolean shouldFilter() {
         if (blackIPs.isEmpty()) {
@@ -45,21 +52,35 @@ public class BlackIPAccessFilter extends ZuulFilter {
         return blackIPs.contains(ip);// 判断ip是否在黑名单列表里
     }
 
+    /**
+     * 过滤器的具体逻辑
+     */
     @Override
     public Object run() {
         RequestContext requestContext = RequestContext.getCurrentContext();
         requestContext.setResponseStatusCode(HttpStatus.FORBIDDEN.value());
         requestContext.setResponseBody("black ip");
         requestContext.setSendZuulResponse(false);
-
         return null;
     }
 
+    /**
+     * 过滤的优先级，数字越大，优先级越低。
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
 
+
+    /**
+     * 前置过滤器。
+     * 但是在 zuul 中定义了四种不同生命周期的过滤器类型：
+     * 1、pre：请求在路由之前被调用,如:身份验证；
+     * 2、route：请求在路由时被调用；
+     * 3、post：路由到微服务之后执行(在route和error过滤器之后被调用)；
+     * 4、error：处理请求发生错误时被调用；
+     */
     @Override
     public String filterType() {
         return FilterConstants.PRE_TYPE;
